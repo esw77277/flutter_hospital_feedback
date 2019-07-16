d47e55cdedcf5672bc7ee34f44ce9bef6e77b408
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-07-16 16:44:41 +0530

    Update rating_images.dart

--- flutter_login_demo-master/lib/pages/rating_images.dart
+++ flutter_login_demo-master/lib/pages/rating_images.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login_demo/services/authentication.dart';
 import 'login_signup_page.dart';
 
-
-
-
-
 class Profile extends StatefulWidget {
   @override
   _AboutPageState createState() => new _AboutPageState();
@@ -30,27 +26,24 @@ class _AboutPageState extends State<Profile> {
           backgroundColor: const Color(0xFF0099a9),
         ),
         body: Column(children: <Widget>[
-          // Text('The length of time that i had to wait to be seen was reasonable'),
-          //     margin: const EdgeInsets.only(top: 10.0),
           Padding(
             padding: const EdgeInsets.all(30),
             child: new Text(
               'The length of time that i had to wait to be seen was reasonable',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
           ),
-
           Row(
             //ROW 1
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.all(29.0),
+                padding: EdgeInsets.all(23.0),
               ),
 
               GestureDetector(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/highlidisatiifes.png'),
+                    Image.asset('assets/highlidisatiifes.png', height: 50),
                     Text('Highly DiSatiesfied'),
                   ],
                 ),
@@ -66,7 +59,7 @@ class _AboutPageState extends State<Profile> {
               GestureDetector(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/disatidfied.png'),
+                    Image.asset('assets/disatidfied.png', height: 50),
                     Text('Dis-Satiesfied'),
                   ],
                 ),
@@ -79,7 +72,7 @@ class _AboutPageState extends State<Profile> {
               GestureDetector(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/niethersatnordis.png'),
+                    Image.asset('assets/niethersatnordis.png', height: 50),
                     Text('Niether-Satisfied'),
                   ],
                 ),
@@ -91,16 +84,23 @@ class _AboutPageState extends State<Profile> {
               ),
             ],
           ),
+
+           Padding(
+                    padding: const EdgeInsets.only(top: 44.0),
+         ),
           Row(//ROW 2
               children: [
             Padding(
-              padding: EdgeInsets.all(29.0),
+              padding: EdgeInsets.only(left: 54),
             ),
             GestureDetector(
               child: Column(
                 children: <Widget>[
-                  Image.asset('assets/satisfied.png'),
+                  Image.asset('assets/satisfied.png', height: 50),
                   Text('Satisfied'),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 109.0),
+                  ),
                 ],
               ),
               onTap: () {
@@ -112,8 +112,11 @@ class _AboutPageState extends State<Profile> {
             GestureDetector(
               child: Column(
                 children: <Widget>[
-                  Image.asset('assets/highlysatisied.png'),
+                  Image.asset('assets/highlysatisied.png', height: 50),
                   Text('highlysatisied'),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 94.0),
+                  ),
                 ],
               ),
               onTap: () {
@@ -125,7 +128,7 @@ class _AboutPageState extends State<Profile> {
             GestureDetector(
               child: Column(
                 children: <Widget>[
-                  Image.asset('assets/notapplicable.png'),
+                  Image.asset('assets/notapplicable.png', height: 50),
                   Text('NotApplicable'),
                 ],
               ),
@@ -136,11 +139,22 @@ class _AboutPageState extends State<Profile> {
               },
             )
           ]),
+          Padding(
+                    padding: const EdgeInsets.only(top: 34.0),
+         ),
           Text(msg),
 
+         Padding(
+                    padding: const EdgeInsets.only(top: 74.0),
+         ),
           new RaisedButton(
-            child: const Text('Next',style: TextStyle(color: Colors.white),),
+  
+            child: const Text(
+              'Next',
+              style: TextStyle(color: Colors.white),
+            ),
             color: Theme.of(context).accentColor,
+                  
             elevation: 4.0,
             splashColor: Colors.blueGrey,
             onPressed: () {
