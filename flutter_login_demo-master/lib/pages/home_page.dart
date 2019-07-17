6791211a008371fdac19e10594296aae676b8f01
raja-kambala <raja.snpl@gmail.com>
2019-07-17 12:43:35 +0530

    home

--- flutter_login_demo-master/lib/pages/home_page.dart
+++ flutter_login_demo-master/lib/pages/home_page.dart
@@ -6,12 +6,12 @@ import 'dart:async';
 import 'package:flutter_login_demo/pages/firebase.dart';
 import 'package:flutter_login_demo/pages/rating.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'rating.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'login_signup_page.dart';
 import 'thankyou.dart';
 import 'package:grouped_buttons/grouped_buttons.dart';
-
-void main() => runApp(HomePage());
+import './normal_question.dart';
+import './multiple.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.auth, this.userId, this.onSignedOut})
@@ -21,18 +21,16 @@ class HomePage extends StatefulWidget {
   final VoidCallback onSignedOut;
   final String userId;
 
+
   @override
   State<StatefulWidget> createState() => new _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   double _rating;
-  String msg = '';
-  String color = '', nocolor = '';
-
   bool _isRTLMode = false;
-  double _index = 0;
-  int _count = 0;
+  double _index =0;
+  int _count =0;
   String _uemail;
   final FirebaseDatabase _database = FirebaseDatabase.instance;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -89,7 +87,7 @@ class _HomePageState extends State<HomePage> {
         return AlertDialog(
           title: new Text("Verify your account"),
           content:
-          new Text("Link to verify account has been sent to your email"),
+              new Text("Link to verify account has been sent to your email"),
           actions: <Widget>[
             new FlatButton(
               child: new Text("Dismiss"),
@@ -112,39 +110,30 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-
-
-  onclicked() {
-    setState(() {
-      msg;
-    });
-  }
-
-  createData() {
-    DocumentReference ds =
-    Firestore.instance.collection('User_Ratings').document(_uemail);
-    Map<String, dynamic> rdata = {
-      "Question_ID": _count,
+  createData(){
+    DocumentReference ds = Firestore.instance.collection('User_Ratings').document(_uemail);
+    Map<String,dynamic> rdata={
+      "Question_ID":_count,
       "Rating": _rating,
-      "User_Email": _uemail,
-    };
-    ds.setData(rdata).whenComplete(() {
-      print("Submitted Successful");
-    });
-  }
+      "User_Email":_uemail,
 
-  TextEditingController _textFieldController = TextEditingController();
+    };
+    ds.setData(rdata).whenComplete((){print("Submitted Successful");});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return new
+
+    Scaffold(
         appBar: new AppBar(
           title: new Text('Feedback'),
           actions: <Widget>[
             new FlatButton(
                 child: new Text('Logout',
                     style: new TextStyle(fontSize: 17.0, color: Colors.white)),
-                onPressed: () {
+                onPressed: ()
+                {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => LoginSignUpPage()),
@@ -153,180 +142,123 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         body: Container(
-            child: Center(
-              child: FittedBox(
-                fit: BoxFit.contain,
-                child: StreamBuilder(
-                    stream: Firestore.instance.collection('Questions').snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        //print('snapshot data $snapshot.data');
-                        return Text('Loading Data... Please Wait');
-                      }
-                      //_question = snapshot.data.documents[index]['Question']
+          child: Center(
+            child: StreamBuilder(
+                stream: Firestore.instance.collection('Questions').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return Text('Loading Data... Please Wait');
+                  //_question = snapshot.data.documents[index]['Question']
+                  return //Row(
+                      //children: <Widget>[
+                      Column(
 
-                      return //Row(
-                        //children: <Widget>[
-                        Column(children: <Widget>[
-                          SizedBox(
-                            height: 200.0,
-                            width: 50.0,
-                          ),
-                          /*Text(
+                    children: <Widget> [
+
+                      SizedBox(
+                        height: 200.0,
+                        width: 50.0,
+                      ),
+                      /*Text(
                           snapshot.data.documents[0]['Category'],
                           textAlign: TextAlign.left,
                           style: TextStyle(fontSize: 30),
                         ),*/
-                          Text(
-                            snapshot.data.documents[_count]['Question'],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 25),
-                          ),
-                          _count == 5
-                              ? CheckboxGroup(
-                            labels: <String>[
-                              snapshot.data.documents[_count]['Option1'],
-                              snapshot.data.documents[_count]['Option2'],
-                              snapshot.data.documents[_count]['Option3'],
-                              snapshot.data.documents[_count]['Option4'],
-                              snapshot.data.documents[_count]['Option5'],
-                            ],
-                            disabled: [
-                              snapshot.data.documents[_count]['Option6'],
-                            ],
-                            onChange: (bool isChecked, String label,
-                                int index) =>
-                                print(
-                                    "isChecked: $isChecked   label: $label  index: $index"),
-                            onSelected: (List<String> checked) =>
-                                print("checked: ${checked.toString()}"),
-                          )
-                              : /*new TextField(
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 3,
-                          ),*/
-
-                          Row(
-                            //ROW 1
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.all(23.0),
-                              ),
-                              GestureDetector(
-                                child: Column(
-                                  children: <Widget>[
-                                    Image.asset('images/1.png'),
-                                    Text('Highly DiSatiesfied'),
-                                  ],
-                                ),
-                                onTap: () {
-                                  msg = 'Highly DiSatisfied';
-                                  onclicked();
-                                  print(msg);
-                                },
-                              ),
-                              // ),
-                              GestureDetector(
-                                child: Column(
-                                  children: <Widget>[
-                                    Image.asset('images/2.png'),
-                                    Text('Dis-Satiesfied'),
-                                  ],
-                                ),
-                                onTap: () {
-                                  msg = 'Disatisfied';
-                                  onclicked();
-                                  print(msg);
-                                },
-                              ),
-                              GestureDetector(
-                                child: Column(
-                                  children: <Widget>[
-                                    Image.asset('images/3.png'),
-                                    Text('Niether-Satisfied'),
-                                  ],
-                                ),
-                                onTap: () {
-                                  msg = 'niether Satisfied nor';
-                                  onclicked();
-                                  print(msg);
-                                },
-                              ),
+                      if(_count==5)
+                      MulQue()
 
 
-                          Row(//ROW 2
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.all(30.0),
-                                ),
-                                GestureDetector(
-                                  child: Column(
-                                    children: <Widget>[
-                                      Image.asset('images/4.png'),
-                                      Text('Satisfied'),
-                                    ],
-                                  ),
-                                  onTap: () {
-                                    msg = 'Satisfied';
-                                    onclicked();
-                                    print(msg);
-                                  },
-                                ),
-                                GestureDetector(
-                                  child: Column(
-                                    children: <Widget>[
-                                      Image.asset('images/5.png'),
-                                      Text('highlysatisied'),
-                                    ],
-                                  ),
-                                  onTap: () {
-                                    msg = 'highlysatisied';
-                                    onclicked();
-                                    print(msg);
-                                  },
-                                ),
-                                GestureDetector(
-                                  child: Column(
-                                    children: <Widget>[
-                                      Image.asset('images/6.png'),
-                                      Text('NotApplicable'),
-                                    ],
-                                  ),
-                                  onTap: () {
-                                    msg = 'notapplicable';
-                                    onclicked();
-                                    print(msg);
-                                  },
-                                )
-                              ]),
-                            ],
-                          ),
-                          Text(msg),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          Center(
-                              child: RaisedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    if (_count == 5) {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => Thanks()),
-                                      );
-                                    } else {
-                                      _count++;
-                                      _textFieldController = null;
-                                      createData();
-                                    }
-                                  });
-                                },
-                                child: Text("Next"),
-                                color: Colors.lightBlueAccent,
-                              )),
-                        ]);
-                    }),
-              ),
-            )));
+                      else
+                        NormalQue()
+
+                      /*Text(
+
+                        snapshot.data.documents[_count]['Question'],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 25),
+
+                      ),*/
+                      /*CheckboxGroup(
+                        labels: <String>[
+                          snapshot.data.documents[_count]['Option1'],
+                          snapshot.data.documents[_count]['Option2'],
+                          snapshot.data.documents[_count]['Option3'],
+                          snapshot.data.documents[_count]['Option4'],
+                          snapshot.data.documents[_count]['Option5'],
+                          snapshot.data.documents[_count]['Option6'],
+
+                        ],
+                        *//*disabled: [
+                                "Wednesday",
+                                "Friday"
+                              ],*//*
+                        onChange: (bool isChecked, String label, int index) => print("isChecked: $isChecked   label: $label  index: $index"),
+                        onSelected: (List<String> checked) => print("checked: ${checked.toString()}"),
+                      ),*/
+                      /*FlutterRatingBar(
+                        initialRating: _index,
+                        allowHalfRating: false,
+                        ignoreGestures: false,
+                        tapOnlyMode: false,
+                        itemCount: 5,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                        onRatingUpdate: (rating) {
+                          setState(() {
+                            //_rating = rating;
+                            rating = 0;
+                            //_index=0;
+                          });
+                        },
+                      ),*/
+                      /*SizedBox(
+                        height: 20.0,
+                      ),
+                      Center(
+                  child: RaisedButton(
+                  onPressed: (){
+                    setState((){
+
+                      _count++;
+
+                      createData();
+                      if(_count < 6){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Thanks()),
+                        );
+                      };
+
+
+                    });
+
+
+                },
+                  child: Text("Next"),
+                  color: Colors.lightBlueAccent,
+                  )*/
+                  //),
+                  ]
+                      /* _rating != null
+                            ? Text(
+                          "Rating: $_rating",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                            : Container(),*/
+                      //MyAppRating(),
+                      /*Text(
+                          snapshot.data.documents[1]['Category'],
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 30),
+                        ),*/
+                      );
+
+
+
+                  //],
+                  //);
+                }),
+          ),
+        ));
   }
 }
+
